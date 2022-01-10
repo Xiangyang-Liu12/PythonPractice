65250c11596070ea7d580b2870e76091b6be71b4
xiangyang <xiangyang9812@gmail.com>
2022-01-09 23:53:50 -0800

    commit

---
+++ practic1.py
@@ -0,0 +1,31 @@
+from math import *
+
+name = "Xiangyang Liu"
+print(name + " is very cool")
+print(name.lower())
+print(name.upper())
+print(name.isupper())
+print(name.upper().isupper())
+print(len(name))
+print(name[0])
+print(name[6])
+print(name.index("y"))
+print(name.index("Liu"))
+print(name.replace("Xiangyang", "Olivia"))
+
+# Working with numbers
+print(6 + 6.09 * 12)
+print(10 % 3)
+my_num = -5
+# to print a number next to a string, you have to use srt() function
+print(str(my_num) + " is my favority number")
+print(abs(my_num))
+print(pow(2, 3))
+print(max(8, 0))
+print(round(9.678697))
+print(floor(67.8796))
+print(ceil(67.76896))
+print(sqrt(49))
+
+# Getting input from user
+
